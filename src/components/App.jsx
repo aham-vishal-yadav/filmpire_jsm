e2fa12313692eacef43515b6c8a9972082e9c9f2
Vishal Yadav <dit.vishalkumar@gmail.com>
2023-03-24 02:11:10 +0530

    File and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route , Routes } from 'react-router-dom';
+import {MovieInformation , Movies , Actors , Profile , NavBar} from './';
+import useStyles from './styles'
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-        <h1>Hello World</h1>
+    <div className={classes.root}>
+    <CssBaseline />
+    <NavBar />
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Routes>
         
+      <Route path="/movie/:id" element={
+  <Movies />
+} />
+
+<Route path="/actors/:id" element={
+  <Actors />
+} />
+
+<Route path="/" element={
+  <Movies />
+} />
+
+<Route path="/profile/:id" element={
+  <Profile />
+} />
+
+       </Routes>
+    
+    </main>
     </div>
   )
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NavBar = () => {
+    console.log('navBar');
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
